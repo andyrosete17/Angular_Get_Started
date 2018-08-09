7c0dc0440b0f1c74ffe5c533afcf19aeeeb99c6c
unknown <andyrosete17@gmail.com>
2018-08-10 01:25:58 +0200

    First Commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '../../node_modules/@angular/core';
+
+@Component({
+  selector: 'pm-root',
+  templateUrl: 'app.component.html'
+})
+export class AppComponent {
+  pageTittle = 'Acme Product Management';
+}
---
+++ src/app/products/products-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '../../../node_modules/@angular/core';
+
+@Component({
+    selector: 'pm-products',
+    templateUrl: './products-list.component.html'
+})
+export class ProductListComponent {
+pageTitle = 'Product List';
+}

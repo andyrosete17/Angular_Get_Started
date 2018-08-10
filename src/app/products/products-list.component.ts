c0eaeb3203db1650efc8d64b6b30049036fe334a
Andy Rosete <andy.rosete@innovation.group>
2018-08-10 13:59:13 +0200

    Added product list and component validation

--- src/app/products/products-list.component.ts
+++ src/app/products/products-list.component.ts
@@ -5,5 +5,19 @@ import { Component } from '../../../node_modules/@angular/core';
     templateUrl: './products-list.component.html'
 })
 export class ProductListComponent {
-pageTitle = 'Product List';
+    pageTitle = 'Product List';
+    imageWidth = 50;
+    imageMargin = 2;
+    products: any[] = [
+        {
+            'productId': 2,
+            'productName' : 'Garden Cart',
+            'code' : 'GDF-002',
+            'releaseDate' : 'March 18, 2016',
+            'description' : 'Crazy stupid shit',
+            'price' : 35.99,
+            'starRating' : 4.2,
+            'imageUrl': 'https://images-na.ssl-images-amazon.com/images/I/7104mznFWcL._SL1500_.jpg'
+        }
+    ];
 }

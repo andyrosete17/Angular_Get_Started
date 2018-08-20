3f9564b4a1182d5d0701a30575c05fdac3275413
unknown <andyrosete17@gmail.com>
2018-08-20 07:25:42 +0200

    Fixing bugs,

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Route } from '@angular/compiler/src/core';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -8,9 +10,26 @@ import { IProduct } from './product';
 export class ProductDetailComponent implements OnInit {
   pageTitle = 'Product Detail';
   product: IProduct;
-  constructor() { }
+  constructor(private route: ActivatedRoute,
+              private router: Router) { }
 
   ngOnInit() {
+    let id = +this.route.snapshot.paramMap.get('id');
+    this.pageTitle += `: ${id}`;
+    this.product = {
+    'productId': id,
+    'productName': 'Leaf Rake',
+    'code': 'GDN-0011',
+    'releaseDate': 'March 19, 2016',
+    'description': 'Leaf rake with 48-inch wooden handle.',
+    'price': 19.95,
+    'starRating': 3.2,
+    'imageUrl': 'https://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png'
+    }
+  }
+
+  onBack(): void {
+    this.router.navigate(['/products']);
   }
 
 }
